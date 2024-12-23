cc024d61c9c086305b6d805400c1b05949bbceef
edgars <edgars.mintals@gmail.com>
2024-12-23 21:06:06 +0200

    homework 9 , adding method put,delete and new get

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -68,3 +68,101 @@ test('post order with correct data should receive code 400', async ({ request })
   console.log('response body:', await response.text())
   expect(response.status()).toBe(400)
 })
+test('put order with correct data should receive code 200', async ({ request }) => {
+  // prepare request body
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a POST request to the server
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/5', {
+    data: requestBody,
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+test('put order with incorrect data should receive code 400', async ({ request }) => {
+  // prepare request body
+  const requestBody = {
+    status: 'OPEN',
+    courierId: 0,
+    customerName: 'string',
+    customerPhone: 'string',
+    comment: 'string',
+    id: 0,
+  }
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a POST request to the server
+  const response = await request.put('https://backend.tallinn-learning.ee/test-orders/11', {
+    data: requestBody,
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+test('delete order with correct data should receive code 200', async ({ request }) => {
+  // prepare request body
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a POST request to the server
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/5', {
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  expect(response.status()).toBe(StatusCodes.NO_CONTENT)
+})
+test('delete order with incorrect data should receive code 400', async ({ request }) => {
+  // prepare request body
+  const requestHeaders = {
+    api_key: '1234567890123456',
+  }
+  // Send a POST request to the server
+  const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/0', {
+    headers: requestHeaders,
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+test('get order with correct username and password should receive code 200', async ({
+  request,
+}) => {
+  // Build and send a GET request to the server
+  const response = await request.get(
+    'https://backend.tallinn-learning.ee/test-orders?username=ed&password=min',
+  )
+  // Log the response status, body and headers
+  console.log('response body:', await response.json())
+  console.log('response headers:', response.headers())
+  // Check if the response status is 200
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+test('get order with correct username and incorrect password should receive code 400', async ({
+  request,
+}) => {
+  // Build and send a GET request to the server
+  const response = await request.delete(
+    'https://backend.tallinn-learning.ee/test-orders?username=ed&password=min',
+  )
+  // Log the response status, body and headers
+  console.log('response body:', await response.json())
+  console.log('response headers:', response.headers())
+  // Check if the response status is 200
+  expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+})
