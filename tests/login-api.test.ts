41b4cee9f2d20bf8d713895dad64944c2d0916b0
edgars <edgars.mintals@gmail.com>
2024-12-25 15:34:29 +0200

    homework 11: adding a new negative test and upgrading positive test

--- tests/login-api.test.ts
+++ tests/login-api.test.ts
@@ -12,11 +12,9 @@ test.describe('Tallinn delivery API tests', () => {
 
     expect(response.status()).toBe(200)
 
-    const responseBody = await response.text();
-    console.log('Response body:', responseBody);
-    expect(responseBody).toMatch(
-      /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
-    )
+    const responseBody = await response.text()
+    console.log('Response body:', responseBody)
+    expect(responseBody).toMatch(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/)
   })
   test('login with incorrect data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithIncorrectData()
@@ -27,12 +25,12 @@ test.describe('Tallinn delivery API tests', () => {
     expect(response.status()).toBe(401)
   })
   test('login with incorrect HTTP method', async ({ request }) => {
-    const requestBody = LoginDto.createLoginWithCorrectData();
+    const requestBody = LoginDto.createLoginWithCorrectData()
     const response = await request.get(`${serviceURL}${loginPath}`, {
       data: requestBody,
-    });
-    expect(response.status()).toBe(405);
-    const responseBody = await response.text();
-    console.log('Response body:', responseBody);
-  });
+    })
+    expect(response.status()).toBe(405)
+    const responseBody = await response.text()
+    console.log('Response body:', responseBody)
+  })
 })
