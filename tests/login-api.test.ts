720b6d86d88d728b9ded56a243dd5021c82456db
edgars <edgars.mintals@gmail.com>
2024-12-25 15:31:26 +0200

    homework 11: adding a new negative test and upgrading positive test

--- tests/login-api.test.ts
+++ tests/login-api.test.ts
@@ -11,6 +11,12 @@ test.describe('Tallinn delivery API tests', () => {
     })
 
     expect(response.status()).toBe(200)
+
+    const responseBody = await response.text();
+    console.log('Response body:', responseBody);
+    expect(responseBody).toMatch(
+      /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+    )
   })
   test('login with incorrect data', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithIncorrectData()
@@ -20,4 +26,13 @@ test.describe('Tallinn delivery API tests', () => {
 
     expect(response.status()).toBe(401)
   })
+  test('login with incorrect HTTP method', async ({ request }) => {
+    const requestBody = LoginDto.createLoginWithCorrectData();
+    const response = await request.get(`${serviceURL}${loginPath}`, {
+      data: requestBody,
+    });
+    expect(response.status()).toBe(405);
+    const responseBody = await response.text();
+    console.log('Response body:', responseBody);
+  });
 })
